278e1c637c5dd207dea1ec1b4c7eb3d13c95817e
guivmc <gui115vini@hotmail.com>
2025-10-07 20:10:55 -0300

    x and y axis

--- MusicAI.py
+++ MusicAI.py
@@ -13,6 +13,9 @@ from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense,Dropout,Activation
 from keras.optimizers import Adam
+from keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 def plot_dataset(df):
     print(df['Class'].value_counts())
@@ -26,6 +29,17 @@ def mfccs_feature_extractor(file_name):
     mfccs_features=librosa.feature.mfcc(y=audio,sr=sample_rate,n_mfcc=40)
     mfccs_scaled_features =np.mean(mfccs_features.T,axis=0)
     return mfccs_scaled_features
+
+def extract_features(dataset):
+    extracted_features=[]
+    for _,row in tqdm(dataset.iterrows()):
+        file_name=os.path.join(os.path.abspath("Dataset/Musicas/Train_submission/Train_submission"),str(row["FileName"]))
+        final_class_label=row["Class"]
+        mfccs_extrateced=mfccs_feature_extractor(file_name)
+        extracted_features.append([mfccs_extrateced,final_class_label])
+    
+    extracted_features_dataframe=pd.DataFrame(extracted_features,columns=['features','class'])
+    return extracted_features_dataframe
 
 def main():
     parser=argparse.ArgumentParser()
@@ -42,15 +56,24 @@ def main():
     elif args.plot == "test":
         plot_dataset(dataset_test)
 
-    extracted_features=[]
-    for _,row in tqdm(dataset_train.iterrows()):
-        file_name=os.path.join(os.path.abspath("Dataset/Musicas/Train_submission/Train_submission"),str(row["FileName"]))
-        final_class_label=row["Class"]
-        mfccs_extrateced=mfccs_feature_extractor(file_name)
-        extracted_features.append([mfccs_extrateced,final_class_label])
+    dataset_features=extract_features(dataset_train)
+    print(dataset_features.head(5))
 
-    extracted_features_data=pd.DataFrame(extracted_features,columns=['features','class'])
-    print(extracted_features_data.head(5))
+    x_axis=np.array(dataset_features['features'].tolist())
+    y_axis=np.array(dataset_features["class"].tolist())
+
+    LE=LabelEncoder()
+    y_axis=to_categorical(LE.fit_transform(y_axis))
+
+    x_train,x_test,y_train,y_test=train_test_split(x_axis, y_axis,test_size=0.2,random_state=42)
+
+    print("x_train:",x_train.shape)
+    print("x_test:",x_test.shape)
+    print("y_train:",y_train.shape)
+    print("y_test:",y_test.shape)
+
+    num_labels=y_axis.shape[1]
+    print(num_labels)
 
 if __name__ == "__main__":
     main()
